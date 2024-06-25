5e74ed243c257844fc40b010fd4c2541d1824609
xt8512 <xt8512@rimac.com.pe>
2024-06-25 17:03:38 -0500

    feat:eliminando el local storage

--- src/amplify/actions/SignOut.ts
+++ src/amplify/actions/SignOut.ts
@@ -3,6 +3,8 @@ import { Auth } from "@aws-amplify/auth";
 export async function handleSignOut() {
   try {
     await Auth.signOut();
+
+    localStorage.clear();
   } catch (error) {
     console.log("Error signing out: ", error);
   }
